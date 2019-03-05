b964f01ccb42bc79a59a23a9ccdc9e0aa53efb4b
SephVin <vinseph236@gmail.com>
2019-03-05 21:09:06 +0500

    Задание 1. Подготовьте инфраструктуру

---
+++ SeleniumTests/SeleniumTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d79a2e1c-1a9f-4878-92b5-4e36232bb189")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumTests/SeleniumTests/Tests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTests
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void MyFirstTest()
+        {
+            driver.Url = "http://software-testing.ru";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
