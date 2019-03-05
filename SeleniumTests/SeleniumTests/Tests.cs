26459ffa3003b1a90e593c1c535b2ed6ca8f8025
SephVin <vinseph236@gmail.com>
2019-03-05 21:36:59 +0500

    Задание 3. Сделайте сценарий логина

--- SeleniumTests/SeleniumTests/Tests.cs
+++ SeleniumTests/SeleniumTests/Tests.cs
@@ -1,30 +1,51 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 
 namespace SeleniumTests
 {
     public class Tests
     {
-        private IWebDriver driver;
+        private WebDriverWait wait;
+        private IWebDriver webDriver;
 
         [SetUp]
         public void SetUp()
         {
-            driver = new ChromeDriver();
+            webDriver = new ChromeDriver();
+            wait = new WebDriverWait(webDriver, TimeSpan.FromMilliseconds(10));
         }
 
         [Test]
         public void MyFirstTest()
         {
-            driver.Url = "http://software-testing.ru";
+            webDriver.Url = "http://software-testing.ru";
+        }
+
+        [Test]
+        public void LoginAsAdminTest()
+        {
+            webDriver.Url = "http://localhost/litecart/admin/login.php";
+            wait.Until(driver => driver.Title.Equals("My Store"));
+
+            webDriver
+                .FindElement(By.CssSelector("input[name ='username']"))
+                .SendKeys("admin");
+            webDriver
+                .FindElement(By.CssSelector("input[name ='password']"))
+                .SendKeys("admin");
+            webDriver
+                .FindElement(By.CssSelector("button[name='login']"))
+                .Click();
         }
 
         [TearDown]
         public void TearDown()
         {
-            driver.Quit();
-            driver = null;
+            webDriver.Quit();
+            webDriver = null;
         }
     }
 }
