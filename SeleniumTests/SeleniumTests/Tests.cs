67b57026b057654b01e7fa2635f984ba02d38e8e
SephVin <vinseph236@gmail.com>
2019-03-13 23:11:49 +0500

    Задание 7. Сделайте сценарий, проходящий по всем разделам админки

--- SeleniumTests/SeleniumTests/Tests.cs
+++ SeleniumTests/SeleniumTests/Tests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentAssertions;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
@@ -41,11 +42,66 @@ namespace SeleniumTests
                 .Click();
         }
 
+        [Test]
+        public void MenuSectionTest()
+        {
+            webDriver.Url = "http://localhost/litecart/admin/login.php";
+            wait.Until(driver => driver.Title.Equals("My Store"));
+
+            webDriver
+                .FindElement(By.CssSelector("input[name ='username']"))
+                .SendKeys("admin");
+            webDriver
+                .FindElement(By.CssSelector("input[name ='password']"))
+                .SendKeys("admin");
+            webDriver
+                .FindElement(By.CssSelector("button[name='login']"))
+                .Click();
+            wait.Until(driver => driver.FindElement(By.CssSelector(".notice.success")));
+
+            var sectionsCount = webDriver
+                .FindElements(By.CssSelector("li#app-"))
+                .Count;
+
+            for (var i = 0; i < sectionsCount; i++)
+            {
+                webDriver
+                    .FindElements(By.CssSelector("li#app-"))[i]
+                    .Click();
+
+                IsElementPresent(By.CssSelector("td#content h1")).Should().BeTrue();
+
+                var subSectionsCount = webDriver.FindElements(By.CssSelector("li#app- li")).Count;
+
+                for (var j = 0; j < subSectionsCount; j++)
+                {
+                    webDriver
+                        .FindElements(By.CssSelector("li#app- li"))[j]
+                        .Click();
+
+                    IsElementPresent(By.CssSelector("td#content h1")).Should().BeTrue();
+                }
+            }
+        }
+
         [TearDown]
         public void TearDown()
         {
             webDriver.Quit();
             webDriver = null;
+        }
+
+        private bool IsElementPresent(By locator)
+        {
+            try
+            {
+                webDriver.FindElement(locator);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
         }
     }
 }
